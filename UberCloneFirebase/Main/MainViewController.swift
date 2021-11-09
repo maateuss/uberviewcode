59eba1fd7f3aed5f19958952df667abb175ebc69
Mateus Moura Santos <msantos.96@outlook.com>
2021-11-09 09:47:00 -0300

    Initial commit

---
+++ UberCloneFirebase/Main/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  UberCloneFirebase
+//
+//  Created by Mateus Santos on 09/11/21.
+//
+
+
+import UIKit
+
+
+class MainViewController : UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .blue
+    }
+    
+}
+
