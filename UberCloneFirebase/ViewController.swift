7890fedb2fce617486b0291d49607909f15c9ebd
Mateus Moura Santos <msantos.96@outlook.com>
2021-11-09 09:29:40 -0300

    Initial Commit

---
+++ UberCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberCloneFirebase
+//
+//  Created by Mateus Santos on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
